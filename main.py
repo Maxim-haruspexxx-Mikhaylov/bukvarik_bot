4ff99058b7ffd481b5b32c52f5ee4edb9e1f62ca
Maxim <maximmikhaylov00@gmail.com>
2021-04-23 02:06:10 +0300

    First links

--- main.py
+++ main.py
@@ -1,27 +1,100 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
+from telegram import ReplyKeyboardMarkup, ParseMode
+from telegram.utils import helpers
+
+# Напишем соответствующие функции.
+reply_keyboard = [['/textbook', '/tests']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+def textbook(update, context):
+    bot = context.bot
+    url_parts_of_speech = helpers.create_deep_linked_url(bot.username, 'parts-of-speech')
+    url_phonetics = helpers.create_deep_linked_url(bot.username, 'phonetics')
+    url_grammar = helpers.create_deep_linked_url(bot.username, 'grammar')
+    url_lexis = helpers.create_deep_linked_url(bot.username, 'lexis')
+    url_punctuation = helpers.create_deep_linked_url(bot.username, 'punctuation')
+    url_particles_and_conjunctions = helpers.create_deep_linked_url(bot.username, 'particles-and-conjunctions')
+    url_orphography = helpers.create_deep_linked_url(bot.username, 'orphography')
+    text = f'''Справочник:
+        
+    [Части речи]({url_parts_of_speech})
+    
+    [Фонетика]({url_phonetics})
+    
+    [Грамматика]({url_grammar})
+    
+    [Лексика]({url_lexis})
+    
+    [Пунктуация]({url_punctuation})
+    
+    [Частицы и союзы]({url_particles_and_conjunctions})
+    
+    [Орфография]({url_orphography})
+    '''
+    update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, сам бот и класс updater, принявший сообщение.
-def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    update.message.reply_text(update.message.text)
+
+
+def in_parts_of_speech():
+    update.message.reply_text('да')
+    pass
+
+
+def in_phonetics():
+    pass
+
+
+def in_grammar():
+    pass
+
+
+def in_lexis():
+    pass
+
+
+def in_punctuation():
+    pass
+
+
+def in_particles_and_conjunctions():
+    pass
+
+
+def in_orphography():
+    pass
+
+
+def start(update, context):
+    update.message.reply_text(
+        "Я бот-справочник. Какая информация вам нужна?",
+        reply_markup=markup
+    )
     print('Done!')
 
 
+# def deep_linked_level_2(update, context) -> None:
+#    """Reached through the SO_COOL payload"""
+#    bot = context.bot
+#    url = helpers.create_deep_linked_url(bot.username, 'nothing')
+#    text = f"[Части речи]({url})."
+#    update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
 def main():
-    TOKEN = '1797940789:AAENadc1txh59h5FX3Kd_DMUrBECpiYwu9g'
+    token = '1797940789:AAENadc1txh59h5FX3Kd_DMUrBECpiYwu9g'
 
-    # Создаём объект updater.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(token, use_context=True)
 
-    # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
 
     # Создаём обработчик сообщений типа Filters.text
@@ -29,11 +102,21 @@ def main():
     # После регистрации обработчика в диспетчере
     # эта функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(Filters.text, echo)
 
     # Регистрируем обработчик в диспетчере.
-    dp.add_handler(text_handler)
+
+    dp.add_handler(CommandHandler("textbook", textbook))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("close", close_keyboard))
+    dp.add_handler(CommandHandler("start", in_parts_of_speech, Filters.regex('parts-of-speech')))
+    dp.add_handler(CommandHandler("start", in_phonetics, Filters.regex('phonetics')))
+    dp.add_handler(CommandHandler("start", in_grammar, Filters.regex('grammar')))
+    dp.add_handler(CommandHandler("start", in_lexis, Filters.regex('lexis')))
+    dp.add_handler(CommandHandler("start", in_punctuation, Filters.regex('punctuation')))
+    dp.add_handler(CommandHandler("start", in_particles_and_conjunctions, Filters.regex('particles-and-conjunctions')))
+    dp.add_handler(CommandHandler("start", in_orphography, Filters.regex('orphography')))
     # Запускаем цикл приема и обработки сообщений.
+
     updater.start_polling()
 
     # Ждём завершения приложения.
