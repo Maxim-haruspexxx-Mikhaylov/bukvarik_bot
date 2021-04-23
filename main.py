687e0a5542dfdf090d143b1f811a1103f65eb1aa
Maxim <maximmikhaylov00@gmail.com>
2021-04-23 13:55:37 +0300

    Release...till now

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
 from telegram.ext import CallbackContext, CommandHandler
 from telegram import ReplyKeyboardMarkup, ParseMode, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.utils import helpers
+import os
 
-# Напишем соответствующие функции.
 reply_keyboard = [['/textbook', '/tests']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
@@ -15,53 +15,70 @@ def close_keyboard(update, context):
     )
 
 
-def textbook(update, context):
+def textbook_1(update, context):
     bot = context.bot
-
+    names = os.listdir('themes')
     update.message.reply_text(
         "СПРАВОЧНИК",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Части речи", callback_data='parts-of-speech')],
-            [InlineKeyboardButton(text="Фонетика", callback_data='phonetics')],
-            [InlineKeyboardButton(text="Грамматика", callback_data='grammar')],
-            [InlineKeyboardButton(text="Лексика", callback_data='lexis')],
-            [InlineKeyboardButton(text="Пунктуация", callback_data='punctuation')],
-            [InlineKeyboardButton(text="Частицы и союзы", callback_data='particles-and-conjunctions')],
-            [InlineKeyboardButton(text="Орфография", callback_data='orphography')]]
+            [[InlineKeyboardButton(text=names[0], callback_data='parts-of-speech')],
+            [InlineKeyboardButton(text=names[1], callback_data='phonetics')],
+            [InlineKeyboardButton(text=names[2], callback_data='grammar')],
+            [InlineKeyboardButton(text=names[3], callback_data='lexis')],
+            [InlineKeyboardButton(text=names[4], callback_data='punctuation')],
+            [InlineKeyboardButton(text=names[5], callback_data='particles-and-conjunctions')],
+            [InlineKeyboardButton(text=names[6], callback_data='orphography')]]
         ),
     )
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, сам бот и класс updater, принявший сообщение.
 
 
 def in_parts_of_speech(update, context):
-    bot = context.bot
-    update.callback_query.message.reply_text('да')
+    update.callback_query.message.reply_text('----------------------------')
+    names = os.listdir(os.path.join('themes', '1) Части речи'))
+    update.callback_query.message.reply_text(
+        'Части речи',
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text=names[0], callback_data='noun')],
+            [InlineKeyboardButton(text=names[1], callback_data='verb')]]
+        ),
+    )
+
+
+def in_noun():
+    names = os.listdir(os.path.join('themes', '1) Части речи', '1) Существительное'))
+    f = open('example.txt', 'r')
+    pass
+
+
+def in_verb():
+    pass
+
+
+# --------------------------------------------------------------------------------
 
 
 def in_phonetics(update, context):
-    update.message.reply_text('да')
-    pass
+    update.callback_query.answer('В разработке')
 
 
 def in_grammar(update, context):
-    pass
+    update.callback_query.answer('В разработке')
 
 
 def in_lexis(update, context):
-    pass
+    update.callback_query.answer('В разработке')
 
 
 def in_punctuation(update, context):
-    pass
+    update.callback_query.answer('В разработке')
 
 
 def in_particles_and_conjunctions(update, context):
-    pass
+    update.callback_query.answer('В разработке')
 
 
 def in_orphography(update, context):
-    pass
+    update.callback_query.answer('В разработке')
 
 
 def start(update, context):
@@ -72,14 +89,6 @@ def start(update, context):
     print('Done!')
 
 
-# def deep_linked_level_2(update, context) -> None:
-#    """Reached through the SO_COOL payload"""
-#    bot = context.bot
-#    url = helpers.create_deep_linked_url(bot.username, 'nothing')
-#    text = f"[Части речи]({url})."
-#    update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
-
-
 def main():
     token = '1797940789:AAENadc1txh59h5FX3Kd_DMUrBECpiYwu9g'
 
@@ -87,25 +96,11 @@ def main():
 
     dp = updater.dispatcher
 
-    # Создаём обработчик сообщений типа Filters.text
-    # из описанной выше функции echo()
-    # После регистрации обработчика в диспетчере
-    # эта функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
-
-    # Регистрируем обработчик в диспетчере.
-
-    dp.add_handler(CommandHandler("textbook", textbook))
+    dp.add_handler(CommandHandler("textbook", textbook_1))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("close", close_keyboard))
-#    dp.add_handler(CommandHandler("start", in_parts_of_speech, Filters.regex('parts-of-speech')))
-#    dp.add_handler(CommandHandler("start", in_phonetics, Filters.regex('phonetics')))
-#    dp.add_handler(CommandHandler("start", in_grammar, Filters.regex('grammar')))
-#    dp.add_handler(CommandHandler("start", in_lexis, Filters.regex('lexis')))
-#    dp.add_handler(CommandHandler("start", in_punctuation, Filters.regex('punctuation')))
-#    dp.add_handler(CommandHandler("start", in_particles_and_conjunctions, Filters.regex('particles-and-conjunctions')))
-#    dp.add_handler(CommandHandler("start", in_orphography, Filters.regex('orphography')))
-    # Запускаем цикл приема и обработки сообщений.
+
+    files = os.listdir('themes')
 
     dp.add_handler(CallbackQueryHandler(in_parts_of_speech, pattern='parts-of-speech'))
     dp.add_handler(CallbackQueryHandler(in_phonetics, pattern='phonetics'))
@@ -115,13 +110,13 @@ def main():
     dp.add_handler(CallbackQueryHandler(in_particles_and_conjunctions, pattern='particles-and-conjunctions'))
     dp.add_handler(CallbackQueryHandler(in_orphography, pattern='orphography'))
 
+    dp.add_handler(CallbackQueryHandler(in_noun, pattern='noun'))
+    dp.add_handler(CallbackQueryHandler(in_verb, pattern='verb'))
+
     updater.start_polling()
 
-    # Ждём завершения приложения.
-    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
